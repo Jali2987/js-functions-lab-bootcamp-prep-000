231264375035fb25884eab35222c422e8254843a
jalissa.nugent@yahoo.com <jalissa.nugent@yahoo.com>
2018-06-25 14:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // write your code below!
+
+function happyHolidays() {
+var message = "Happy Holidays!";
+return message; 
+}
+console.log(happyHolidays());
+
+function happyHolidaysTo(name) {
+return message = `Hello, ${name}!`;
+}
+console.log(happyHolidaysTo());
+
+function happyHolidayTo(holiday, name) {
+  console.log(`${holiday}, ${name}!`);
+}
+
+function holidayCountdown(days) {
+  console.log(`It's ${days} days until ${holiday}!`); 
+}
+
+
+/*
+
+
+Two parameters (greeting, firstName)
+function say(greeting, firstName) {
+console.log(`${greeting}, ${firstName}!`)
+}
+
+
+// declares the sayHello function
+function sayHello() {
+  var message = "Hello!"
+  return message; // returns value instead of printing it
+}
+
+// function returns "Hello!" and console.log prints the return value
+console.log(sayHello());
+
+function doSomething(thing) {
+console.log(thing)
+}
+ 
+doSomething('anything')
+----
+
+function sayHelloTo(firstName) {
+console.log(`Hello, ${firstName}!`)
+}
+ 
+sayHelloTo('Isabel') // "Hello, Isabel!"
+
+
+
+*/
